d2ceedc3cb77c864f72c8b02c63c3f882068fcc8
vinhub100 <vinmail17@gmail.com>
2022-01-07 22:00:54 +0530

    Beautiful Signup Form

--- lekhak/main/forms.py
+++ lekhak/main/forms.py
@@ -11,21 +11,26 @@ class SignupForm(forms.Form):
     email = forms.EmailField(label='Email', required=True)
     password = forms.CharField(label='Password', required=True)
     confirn_password = forms.CharField(label='Confirm Password', required=True)
-    signup_date = forms.DateField(label='Today Date', widget=forms.SelectDateWidget(), required=True)
+    signup_date = forms.DateField(label='Today Date', widget=forms.SelectDateWidget(years=range(1920,2023)), required=True)
 
     @property
     def helper(self):
         helper = FormHelper()
 
         helper.layout = Layout(
-            HTML('<h2 class="text-center">Signup Form</h2>'),
-            Field('firstname'),
-            Field('lastname'),
-            Field('email'),
-            Field('password'),
-            Field('confirn_password'),
-            Field('signup_date'),
-            Submit('submit','Submit', css_class='btn btn-outline-success float-right')
+            HTML('<h2 class="text-center p-3">Signup Form</h2>'),
+            Field('firstname', wrapper_class='row'),
+            Field('lastname', wrapper_class='row '),
+            Field('email', wrapper_class='row '),
+            Field('password', wrapper_class='row '),
+            Field('confirn_password', wrapper_class='row '),
+            Field('signup_date', wrapper_class='row'),
+            Submit('submit','Register', css_class='btn btn-lg btn-outline-success float-right', style='width="100%"')
         )
+        helper.field_class = 'col-9'
+        helper.label_class = 'col-3'
+        
+
+
 
         return helper
